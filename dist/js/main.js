89d431dd4f4b0a77f62082de34ac01cc344c5201
abdelhakeemdiab <abdelhakeem.diab@gmail.com>
2020-08-19 12:58:57 +0200

    Lesson 12, 13, 14

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,13 @@
 $(function() {
     'use strict';
+    // Toggle Side Bar 
     $('.toggle-sidebar').on('click', function ()  {
         $('.sidebar, .content-area').toggleClass('no-sidebar');
+    });
+    //Toggle Sub Menu
+    $('.toggle-submenu').on('click', function () {
+        $(this).find('.fa-angle-right').toggleClass('down');
+        $(this).next('.child-links').slideToggle();
     });
 });
 const myMap = new Map()
--- stage/js/main.js
+++ stage/js/main.js
@@ -1,6 +1,12 @@
 $(function() {
     'use strict';
+    // Toggle Side Bar 
     $('.toggle-sidebar').on('click', function ()  {
         $('.sidebar, .content-area').toggleClass('no-sidebar');
+    });
+    //Toggle Sub Menu
+    $('.toggle-submenu').on('click', function () {
+        $(this).find('.fa-angle-right').toggleClass('down');
+        $(this).next('.child-links').slideToggle();
     });
 });
